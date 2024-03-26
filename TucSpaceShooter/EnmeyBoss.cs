69f6d6bcf69fabb3f39f26fb576c500184afaa21
omar ogaili <ogailiomar>
2024-03-26 09:48:10 +0100

    Boss movement has been fixed

--- TucSpaceShooter/EnmeyBoss.cs
+++ TucSpaceShooter/EnmeyBoss.cs
@@ -23,40 +23,46 @@ namespace TucSpaceShooter
         }
         public override void MoveToRandomPosition(GraphicsDeviceManager graphics)
         {
-            if (position.Y < graphics.PreferredBackBufferHeight)
+            if (!isDead)
             {
-                position.Y += 1;
-            }
-            else
-            {
-                position.Y = 0;
-            }
-            if (moveBack)
-            {
-                position.Y += movingSpeed;
-            }
-            else
-            {
-                position.Y -= movingSpeed;
-            }
-            if (position.Y <= 20 || position.Y >= graphics.PreferredBackBufferWidth - 60)
-            {
-                moveBack = !moveBack;
-            }
-            if (moveRight)
-            {
-                position.X += movingSpeed;
+                if (position.Y < graphics.PreferredBackBufferHeight)
+                {
+                    position.Y += 1;
+                }
+                else
+                {
+                    position.Y = 0;
+                }
 
-            }
-            else
-            {
-                position.X -= movingSpeed;// attacing 
-                position.Y -= movingSpeed;//rolling back
-                position.X -= movingSpeed;// last attack
-            }
-            if (position.X <= 20 || position.X >= graphics.PreferredBackBufferWidth - 60)
-            {
-                moveRight = !moveRight;
+                if (moveBack)
+                {
+                    position.Y += movingSpeed;
+                }
+                else
+                {
+                    position.Y -= movingSpeed;
+                }
+
+                if (position.Y <= 20 || position.Y >= graphics.PreferredBackBufferHeight - 60)
+                {
+                    moveBack = !moveBack;
+                }
+
+                if (moveRight)
+                {
+                    position.X += movingSpeed;
+                }
+                else
+                {
+                    position.X -= movingSpeed; // attacing 
+                    position.Y -= movingSpeed; // rolling back
+                    position.X -= movingSpeed; // last attack
+                }
+
+                if (position.X <= 20 || position.X >= graphics.PreferredBackBufferWidth - 60)
+                {
+                    moveRight = !moveRight;
+                }
             }
         }
 
